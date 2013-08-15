3c9b970538de36ebdf903421df8b0d0a8c31f451
cmcnelis <mcnelis.chris@gmail.com>
2013-08-15 16:45:36 -0500

    Vehicle field validations

--- demo/spec/factories/vehicles.rb
+++ demo/spec/factories/vehicles.rb
@@ -4,13 +4,13 @@ require 'faker'
 
 FactoryGirl.define do
     factory :vehicle do |f|
-        f.vin{Faker::Number.number}
+        f.vin{Faker::Number.digit}
         f.start_date{Date.today}
-        f.expr_date{Data.today + 1.year}
+        f.expr_date{Date.today + 1.year}
         f.make{Faker::Company.name}
         f.model{Faker::Commerce.product_name}
-        f.model{Faker::Commerce.color}
-        f.deductible{Faker::Number.number}
+        f.color{Faker::Commerce.color}
+        f.deductible{Faker::Number.digit}
     end
 end
 
