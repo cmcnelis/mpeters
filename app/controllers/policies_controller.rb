f120d8c215bce9acac38cf44366b2bdf7a1acebe
cmcnelis <mcnelis.chris@gmail.com>
2013-09-11 11:34:16 -0500

    Edit Policy Information

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -25,6 +25,16 @@ class PoliciesController < ApplicationController
         @policy = Policy.find(params[:id])
     end
 
+    def update
+        @policy = Policy.find(params[:id])
+        if @policy.update(policy_params)
+            flash[:notice] = "Successfully updated policy  information."
+            render :action=>'show'
+        else
+            render :action =>'edit'
+        end
+    end
+
     private
         def policy_params
             params.require(:policy).permit!
