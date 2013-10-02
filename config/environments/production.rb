a416586cacc10303c0347877b6858ccbb48179a7
cmcnelis <mcnelis.chris@gmail.com>
2013-10-02 15:58:12 -0500

    SendGrid settings for Heroku environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,14 @@ Demo::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@deductiblesaver.com'}
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
