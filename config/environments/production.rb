294ca8ab4af875f781ad5a4aeb11d599e70f2445
cmcnelis <mcnelis.chris@gmail.com>
2013-10-02 16:09:34 -0500

    More Mailer Settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Demo::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_options = {from: 'no-reply@deductiblesaver.com'}
 
   ActionMailer::Base.smtp_settings = {
