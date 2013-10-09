77270c87d90f5c8ba02f0399a232d63f95146adf
cmcnelis <mcnelis.chris@gmail.com>
2013-10-09 12:36:58 -0500

    Fixed routing in agent controller for embedded login.

--- app/controllers/agent_sessions_controller.rb
+++ app/controllers/agent_sessions_controller.rb
@@ -18,7 +18,7 @@ class AgentSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_back_or_default login_path
+    redirect_back_or_default :root
   end
 
 end
